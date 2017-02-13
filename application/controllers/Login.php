1688593bc2d42092e075b0cbbfec378e0344bf81
Johan Mulyono <dev.jmulyono@gmail.com>
2017-02-13 05:29:48 +0000

    Fixed typo

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,9 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends SQ_Controller {
-
-	public function index()
-	{
+	public function index() {
 		$data = array(
 			'headerCss' => array(
 			),
