d2e114cfb238a9926c7e8cc187eb6dd3e910ea5a
Johan Mulyono <dev.jmulyono@gmail.com>
2017-04-21 10:39:43 +0000

    Fixed css

--- application/views/school_admin_dashboard.php
+++ application/views/school_admin_dashboard.php
@@ -1,17 +1,19 @@
 <div id="sq-school-admin-dashboard-container" class="sq-container">
 	<div class="row school-info">
-		<div class="col-xs-3">
-			<div class="avatar-container" style="background-image: url(<?=($school_admin['school']['photo_url']) ? $school_admin['school']['photo_url'] : $img_path . '/no_image.png'?>)"></div>
-		</div>
-		<div class="col-xs-9">
-			<div class="school-name">
-				<?=$school_admin['school']['name']?>
+		<a href="/school_admin/school_settings">
+			<div class="col-xs-3">
+				<div class="avatar-container" style="background-image: url(<?=($school_admin['school']['photo_url']) ? $school_admin['school']['photo_url'] : $img_path . '/no_image.png'?>)"></div>
 			</div>
-			<div class="school-address">
-				<?=$school_admin['school']['address_1']?><br />
-				<?=$school_admin['school']['city']?>, <?=$school_admin['school']['state']?> <?=$school_admin['school']['zipcode']?>
+			<div class="col-xs-9">
+				<div class="school-name">
+					<?=$school_admin['school']['name']?>
+				</div>
+				<div class="school-address">
+					<?=$school_admin['school']['address_1']?><br />
+					<?=$school_admin['school']['city']?>, <?=$school_admin['school']['state']?> <?=$school_admin['school']['zipcode']?>
+				</div>
 			</div>
-		</div>
+		</a>
 	</div>
 	<div class="row">
 		<div class="col-xs-3">
